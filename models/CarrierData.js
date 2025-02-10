e5e96f9c2c28d5b3982e87cdec73750d882e0185
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2025-02-10 14:36:49 +0500

    now the plan and date is added

--- models/CarrierData.js
+++ models/CarrierData.js
@@ -7,6 +7,7 @@ const CarrierDataSchema = new mongoose.Schema({
     Phone: { type: String, default: "None" },
     USDOT_Number: { type: String, default: "None" },
     Physical_Address: { type: String, default: "None" },
+    plan: { type: String, default: "None" },
     // Mailing_Address: { type: String, default: "None" },
     signature: { type: String, default: "None" },
     MCAuthFile: { type: String, default: "None" },  // Field for MC Authority file path
@@ -18,6 +19,8 @@ const CarrierDataSchema = new mongoose.Schema({
         enum : ["active" , "inActive"],
         default: "active"
     }       // Field for Notice of Violation file path
+}, {
+    timestamps: true // This enables `createdAt` and `updatedAt` fields
 });
 
 // Properly export the Carrier model
